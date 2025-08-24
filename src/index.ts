40de36c359a5b131a2a80b7768f562ddcd76a21c
chaim_cymerman <chaimcymerman0548492309@gmail.com>
2025-08-24 15:36:03 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
 const app = express();
 const port = 3000;
 
+console.log('Starting the server...sssss');
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
