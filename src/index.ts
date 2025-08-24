013182c219f098bcae8fa21424501e565e0d2a16
chaim_cymerman <chaimcymerman0548492309@gmail.com>
2025-08-24 15:21:57 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+﻿console.log('Node עובד!');
+
+console.log('Hello, World!');
+
+
+    const userName = 'Alice';
+
+
+//     git init
+// git add .
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/ChaimCymerman0548492309/test-repo.git
+// git push -u origin main
